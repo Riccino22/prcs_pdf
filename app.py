504a793a5b66afbb9d32353258d6c2a7502df75e
riccino <riccardorinojosam@gmail.com>
2024-08-26 21:05:47 -0300

    add launch.json, and load and process pdf file funcionality

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+import io
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
     st.set_page_config(page_title="Chat with a PDF", page_icon=":books:")
@@ -8,8 +19,13 @@ def main():
     
     with st.sidebar:
         st.subheader("Your document")
-        st.file_uploader("Upload yor PDF file", type="pdf")
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload yor PDF file", type="pdf", accept_multiple_files=True)
+        print(pdf_docs)
+        process = st.button("Process")
+        if process:
+            with st.spinner("Processing"):
+                raw_text =  get_pdf_text(pdf_docs)
+                st.write(raw_text)
 
 
 if __name__ == "__main__":
